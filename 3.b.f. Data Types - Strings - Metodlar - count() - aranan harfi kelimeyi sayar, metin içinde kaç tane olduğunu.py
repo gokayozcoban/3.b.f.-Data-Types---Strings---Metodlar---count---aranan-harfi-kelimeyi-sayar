6eb80e84c6c0f3bce76a8222ca282ed577644f86
gokayozcoban <43710003+gokayozcoban@users.noreply.github.com>
2021-01-09 22:28:20 +0200

    Add files via upload

---
+++ 3.b.f. Data Types - Strings - Metodlar - count() - aranan harfi kelimeyi sayar, metin içinde kaç tane olduğunu.py
@@ -0,0 +1,12 @@
+# count()
+# Kod içerisindeki karakterleri sayar. Diğer metodların aksine count() metodu,
+# parantezlerinin içine sayılması istenilen karakterin girilmesini bekler:
+print("burada kaç tane e harfi var?".count("e"))
+2
+# Bir değişken içerisine de atanabilir:
+metin = """Buraya ayazdığım metin değişkeni içerisine ilk önce ingilizce yazmak
+istedim ama aklıma bi şey gelmedi. Sonra bu yazdığımı ingilizce yazabilirim di-
+ye düşündüm:
+I had want to write here in English what I will write in variable."""
+print(metin.count("e"))
+18
